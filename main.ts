c254e48bc818fb7bd67443f660babbbaac0ed1a0
ju956 <anaaugustonj@gmail.com>
2025-04-02 18:50:47 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,82 @@
+basic.forever(function () {
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Funk), music.PlaybackMode.InBackground)
+    basic.showLeds(`
+        . . # # .
+        . . . # #
+        . . # # .
+        . . . # .
+        . . # # .
+        `)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Funk), music.PlaybackMode.InBackground)
+    basic.showLeds(`
+        . . . # .
+        . . # # #
+        . . . # .
+        . . . # .
+        . . # # .
+        `)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Funk), music.PlaybackMode.InBackground)
+    basic.showLeds(`
+        . . # . .
+        # # # # #
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Funk), music.PlaybackMode.InBackground)
+    basic.showLeds(`
+        . # . . .
+        # # # # #
+        # # . . .
+        # . # . .
+        # . . # .
+        `)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Funk), music.PlaybackMode.InBackground)
+    basic.showLeds(`
+        # . # . .
+        # # # . .
+        . . # . .
+        . . # # .
+        . . # . #
+        `)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Funk), music.PlaybackMode.InBackground)
+    basic.showLeds(`
+        . . # . .
+        # # # # #
+        . . # . .
+        . # . # .
+        . # . # .
+        `)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Funk), music.PlaybackMode.InBackground)
+    basic.showLeds(`
+        . . # . .
+        . # # # #
+        # . # . .
+        . # . . .
+        . # . # .
+        `)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Funk), music.PlaybackMode.InBackground)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . # . # .
+        `)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Funk), music.PlaybackMode.InBackground)
+    basic.showLeds(`
+        # . # . #
+        . # # # .
+        . . # . .
+        . # # # .
+        # . . . #
+        `)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Funk), music.PlaybackMode.InBackground)
+    basic.showLeds(`
+        # . # . #
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+})
